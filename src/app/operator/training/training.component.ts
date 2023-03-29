ba530a5a564769afef2ed0bdc52222bf940c2a16
Ibrahim Mammadli <orkhan.ahmadov123@gmail.com>
2023-03-29 19:19:17 +0400

    operator part 1

--- src/app/operator/training/training.component.ts
+++ src/app/operator/training/training.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from 'src/app/http-service.service';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
-  styleUrls: ['./training.component.scss']
+  styleUrls: ['./training.component.scss'],
 })
 export class TrainingComponent implements OnInit {
+  constructor(private httpService: HttpServiceService) {}
+  modelStatus: any;
+  trainHistory: any;
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  checkModelStatus() {
+    this.httpService
+      .getData('/model_api_connection/list_employees/', {})
+      .subscribe((res) => {
+        this.modelStatus = res;
+      });
   }
-
+  startTrain() {
+    this.httpService
+      .getData('/model_api_connection/list_employees/', {})
+      .subscribe((res) => {
+        this.trainHistory = res;
+      });
+  }
+  startCamera() {}
+  stopCamera() {}
 }
