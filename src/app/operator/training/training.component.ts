b8e416877a43addd4b5de2a1cf1ad8639ac12b3d
Ibrahim Mammadli <orkhan.ahmadov123@gmail.com>
2023-03-29 19:54:50 +0400

    hotfix4673927

--- src/app/operator/training/training.component.ts
+++ src/app/operator/training/training.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpServiceService } from 'src/app/http-service.service';
+import { interval } from 'rxjs';
+import { take, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-training',
@@ -11,6 +13,10 @@ export class TrainingComponent implements OnInit {
   modelStatus: any;
   trainHistory: any;
   cameraStatus: any;
+  frameInfo: any;
+  cam: boolean = false
+  
+
 
   ngOnInit() {}
 
@@ -33,6 +39,7 @@ export class TrainingComponent implements OnInit {
       .getData('/model_api_connection/start_subscription/', {})
       .subscribe((res) => {
         this.cameraStatus = res;
+        this.cam = true;
       });
   }
   stopCamera() {
@@ -40,6 +47,36 @@ export class TrainingComponent implements OnInit {
       .getData('/model_api_connection/stop_subscription/', {})
       .subscribe((res) => {
         this.cameraStatus = res;
+        this.cam = false;
+      });
+  }
+
+  getFrames() {
+    this.httpService
+      .getData('/model_api_connection/start_subscription/', {})
+      .subscribe((res) => {
+        this.frameInfo = res;
+      });
+  }
+
+  sendRequests() {
+    const intervalTime = 100; // 100 milliseconds between requests
+    const requestsPerInterval = 10;
+    const maxRequests = 10000000000000000000;
+    let numRequests = 0;
+  
+    interval(intervalTime)
+      .pipe(
+        takeWhile(() => this.cam == true),
+        take(maxRequests / requestsPerInterval)
+      )
+      .subscribe(() => {
+        for (let i = 0; i < requestsPerInterval; i++) {
+          this.getFrames();
+          if (numRequests >= maxRequests) {
+            break;
+          }
+        }
       });
   }
 }
